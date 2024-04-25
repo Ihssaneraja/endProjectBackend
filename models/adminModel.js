d4dd62a93735b3279b6dfba5ed04dbb5645306bd
chaimaela <laaguidichaimae@gmail.com>
2024-04-25 09:43:28 +0100

    add login seller

--- models/adminModel.js
+++ models/adminModel.js
@@ -7,6 +7,10 @@ const adminSchema = new Schema({
         type: String,
         required: true
     },
+    email:{
+        type: String,
+        required: true
+    },
 
     password:{
         type: String,
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,4 +4,10 @@ const router = require('express').Router()
 
 router.post('/admin-login',authControllers.admin_login)
 
+router.post('/seller-register',authControllers.seller_register)
+
+router.post('/seller-login',authControllers.seller_login)
+
+
+
 module.exports = router
